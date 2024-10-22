64b740150ac0e8e16fc749c2a66262472f420429
Abhilov23 <abhilovgupta01@gmail.com>
2024-10-22 21:32:05 +0530

    Day-2

--- Day-1/script.js
+++ Day-1/script.js
@@ -1,12 +1,13 @@
+//selecting all the queries
 const panels = document.querySelectorAll('.panel')
 
 panels.forEach((panel) => {
-    panel.addEventListener('click', () => {
+    panel.addEventListener('click', () => { /*listens to the events like clicking a button*/ 
         removeActiveClasses()
-        panel.classList.toggle('active')
+        panel.classList.toggle('active') /*classlist.toggle() method is used to add or remove a class from an element */
     })
 })
-
+//function for removing active status from the image 
 function removeActiveClasses() {
     panels.forEach((panel) => {
         panel.classList.remove('active')
